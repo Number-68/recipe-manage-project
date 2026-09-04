be1e47c5d75a5e068af00ad37375931ce5f5b34d
Ahleeryan <ahleeryan@hotmail.com>
2026-09-04 16:00:05 -0500

    halfway done implemented database draft
    tables added: users, directions, recipes, ingredients.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,5 @@
 import { integer, pgTable, varchar, text } from "drizzle-orm/pg-core";
-
+import { defineRelations } from "drizzle-orm";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -9,4 +9,36 @@ export const usersTable = pgTable("users", {
   passwordHash: text().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
 });
+
+
+export const directionsTable = pgTable("directions", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    recipe_id: integer().notNull().references(() => recipesTable.id),
+    recipe_step: integer().notNull(),
+    step_title: varchar().notNull(),
+    description: text().notNull(),
+})
+
+
+export const recipesTable = pgTable("recipes", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  recipe_title: varchar().notNull(),
+  user_id: integer().notNull().references(() => usersTable.id),
+
+
+})
+
+
+
+export const ingredientsTable = pgTable("ingredients", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  recipe_id: integer().notNull().references(() => recipesTable.id),
+  ingredient: varchar({ length:255 }).notNull(),
+  description: text().notNull(),
+
+}
+)
+
+
+
 
